9a54f833a40ac00103688e2fcab1b381a00a9437
Phil <phildev013@gmail.com>
2021-07-16 00:38:50 +0200

    projet_restau_test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bonjour Philippe
         </a>
       </header>
     </div>
